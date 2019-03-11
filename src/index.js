29feb7a8123b29cb6787682852b5b71ba003caec
VeronikaMishkovec <veronikakireenko@gmail.com>
2019-03-11 20:57:30 +0300

    fix: fixed calculate nulls in simple number base

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
 module.exports = function getZerosCount(number, base) {
     // your implementation
     var simpleNumb = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
+    var res = 0;
+    var j = 1;
+    for (var i = 0; i <= simpleNumb.length; i++) {
+        if (base == simpleNumb[i]) {
+            while (Math.pow(simpleNumb[i], j) < number) {
+                res += Math.floor(number / Math.pow(simpleNumb[i], j));
+                j++;
+            }
+        }
 
-    function getZerosCount(number, base) {
-        var res = 0;
-        var j = 1;
+    }
+
+    /*if (base % 2 == 0) {
+        while (base % 2 == 0) {
+            base /= 2;
+        }
+        while (base % 3 == 0) {
+            base /= 3;
+        }
         for (var i = 0; i <= simpleNumb.length; i++) {
             if (base == simpleNumb[i]) {
-                while (Math.pow(simpleNumb[i], j) < number) {
-                    res += Math.floor(number / Math.pow(simpleNumb[i], j));
+                while (Math.pow(simpleNumb[i], j)<number) {
+                    res += Math.floor(number/Math.pow(simpleNumb[i], j));
                     j++;
                 }
             }
-
         }
+    }*/
 
-        /*if (base % 2 == 0) {
-            while (base % 2 == 0) {
-                base /= 2;
-            }
-            while (base % 3 == 0) {
-                base /= 3;
-            }
-            for (var i = 0; i <= simpleNumb.length; i++) {
-                if (base == simpleNumb[i]) {
-                    while (Math.pow(simpleNumb[i], j)<number) {
-                        res += Math.floor(number/Math.pow(simpleNumb[i], j));
-                        j++;
-                    }
-                }
-            }
-        }*/
-
-        return res;
-    }
+    return res;
 }
